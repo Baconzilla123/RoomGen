4c89b3a2a9a394736ecf16c03e54b2baf3009815
Baconzilla123 <105701413+Baconzilla123@users.noreply.github.com>
2022-10-14 13:35:34 +1100

    Create roomgen.py

---
+++ roomgen.py
@@ -0,0 +1,32 @@
+class rooms:
+    class norm:
+        tb = {
+            "l":False,
+            "r":False,
+            "t":True,
+            "b":True,
+        },
+        lr = {
+            "l":True,
+            "r":True,
+            "t":False,
+            "b":False,
+        },
+        br = {
+            "l":False,
+            "r":False,
+            "t":False,
+            "b":True,
+        },
+        bl = {
+            "l":True,
+            "r":False,
+            "t":False,
+            "b":True,
+        },
+        tb = {
+            "l":False,
+            "r":False,
+            "t":False,
+            "b":False,
+        },
